51adf81cee6ff0c56a88f65de7bd27a182fcbad0
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-04-10 21:22:03 +0200

    Created age calculator

---
+++ age-calculator.js
@@ -0,0 +1,6 @@
+const ageCalculator = (thisYear, birthYear) => {
+  console.log(`In ${thisYear} you will be ${thisYear - birthYear} years old.`);
+};
+ageCalculator(2035, 1989);
+ageCalculator(2048, 1989);
+ageCalculator(2056, 1989);
