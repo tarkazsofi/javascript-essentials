8a99826fd90d204ce31d0dd3f4ee9816c4e9ca77
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-04-10 21:42:49 +0200

    Age calculator considers this year

--- age-calculator.js
+++ age-calculator.js
@@ -1,6 +1,17 @@
-const ageCalculator = (thisYear, birthYear) => {
-  console.log(`In ${thisYear} you will be ${thisYear - birthYear} years old.`);
+const now = new Date();
+const thisYear = now.getFullYear();
+const ageCalculator = (targetYear, birthYear) => {
+  const age = targetYear - birthYear;
+  if (targetYear < thisYear) {
+    console.log(`In ${targetYear} you were ${age} years old.`);
+  }
+  if (targetYear === thisYear) {
+    console.log(`This year you are ${age} years old.`);
+  }
+  if (targetYear > thisYear) {
+    console.log(`In ${targetYear} you will be ${age} years old.`);
+  }
 };
-ageCalculator(2035, 1989);
-ageCalculator(2048, 1989);
+ageCalculator(2010, 1989);
+ageCalculator(2021, 1989);
 ageCalculator(2056, 1989);
