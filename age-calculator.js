c18f3c9516a2c5bd5acffac8e28c54b71fb117d6
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-04-11 13:52:47 +0200

    Switched to return statements

--- age-calculator.js
+++ age-calculator.js
@@ -3,15 +3,15 @@ const thisYear = now.getFullYear();
 const ageCalculator = (targetYear, birthYear) => {
   const age = targetYear - birthYear;
   if (targetYear < thisYear) {
-    console.log(`In ${targetYear} you were ${age} years old.`);
+    return `In ${targetYear} you were ${age} years old.`;
   }
   if (targetYear === thisYear) {
-    console.log(`This year you are ${age} years old.`);
+    return `This year you are ${age} years old.`;
   }
   if (targetYear > thisYear) {
-    console.log(`In ${targetYear} you will be ${age} years old.`);
+    return `In ${targetYear} you will be ${age} years old.`;
   }
 };
-ageCalculator(2010, 1989);
-ageCalculator(2021, 1989);
-ageCalculator(2056, 1989);
+console.log(ageCalculator(2010, 1989));
+console.log(ageCalculator(2021, 1989));
+console.log(ageCalculator(2056, 1989));
--- dog-age-calculator.js
+++ dog-age-calculator.js
@@ -3,15 +3,15 @@ const thisYear = now.getFullYear();
 const dogAgeCalculator = (targetYear, birthYear) => {
   const age = (targetYear - birthYear) * 7;
   if (targetYear < thisYear) {
-    console.log(`In ${targetYear} you were ${age} dog years old.`);
+    return `In ${targetYear} you were ${age} dog years old.`;
   }
   if (targetYear === thisYear) {
-    console.log(`This year you are ${age} dog years old.`);
+    return `This year you are ${age} dog years old.`;
   }
   if (targetYear > thisYear) {
-    console.log(`In ${targetYear} you will be ${age} dog years old.`);
+    return `In ${targetYear} you will be ${age} dog years old.`;
   }
 };
-dogAgeCalculator(2010, 1989);
-dogAgeCalculator(2021, 1989);
-dogAgeCalculator(2056, 1989);
+console.log(dogAgeCalculator(2010, 1989));
+console.log(dogAgeCalculator(2021, 1989));
+console.log(dogAgeCalculator(2056, 1989));
