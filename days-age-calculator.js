b0bf1130eec6b1f17921685d2edd832a1c27c2d0
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-04-12 19:53:29 +0200

    Created age in days calculator

---
+++ days-age-calculator.js
@@ -0,0 +1,23 @@
+const now = new Date();
+const currentTimestamp = now.getTime();
+const getAgeInDays = (person) => {
+  const then = new Date(person.birthday);
+  const birthdayTimestamp = then.getTime();
+  const milliSecondsInADay = 1000 * 60 * 60 * 24;
+  const days = Math.floor(
+    (currentTimestamp - birthdayTimestamp) / milliSecondsInADay
+  );
+  return `${person.name} is ${days} days old.`;
+};
+console.log(
+  getAgeInDays({
+    name: "Zsófi",
+    birthday: "1989-01-20",
+  })
+);
+console.log(
+  getAgeInDays({
+    name: "Bence",
+    birthday: "1988-04-19",
+  })
+);
