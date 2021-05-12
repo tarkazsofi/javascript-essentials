2869dec6628444a1d890a70ccdfbb334e933a99a
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-05-12 12:43:57 +0200

    Created plane figure area calculator

---
+++ plane-figures-area-calculator.js
@@ -0,0 +1,33 @@
+const area = (planeFigures) => {
+  if (planeFigures.shape === "circle") {
+    return `${planeFigures.radius ** 2 * Math.PI}`;
+  }
+  if (planeFigures.shape === "square") {
+    return `${planeFigures.side ** 2}`;
+  }
+  if (planeFigures.shape === "rectangle") {
+    return `${planeFigures.horizontalSide * planeFigures.verticalSide}`;
+  }
+};
+
+console.log(
+  area({
+    shape: "circle",
+    radius: 5,
+  })
+);
+
+console.log(
+  area({
+    shape: "square",
+    side: 5,
+  })
+);
+
+console.log(
+  area({
+    shape: "rectangle",
+    horizontalSide: 5,
+    verticalSide: 6,
+  })
+);
