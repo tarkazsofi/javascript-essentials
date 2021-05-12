d5675bca060656013fb709455e11d019d81a8dc5
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-05-12 12:41:42 +0200

    Created average calculator

---
+++ get-average.js
@@ -0,0 +1,7 @@
+const getAverage = (number1, number2) => {
+  return (number1 + number2) / 2;
+};
+
+console.log(getAverage(2, 4)); // Should output 3
+
+console.log(getAverage(10, 20)); // Should output 15
