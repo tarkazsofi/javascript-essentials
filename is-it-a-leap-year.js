90a062dc60b0ef2b9726d15757b8fb08b2308f93
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-05-19 20:23:34 +0200

    Fixed calculation

--- is-it-a-leap-year.js
+++ is-it-a-leap-year.js
@@ -1,5 +1,5 @@
 const isItALeapYear = (year) => {
-  if (year % 4 === 0) {
+  if (year % 4 === 0 && year % 400 === 0) {
     return `${year} is a leap year.`;
   } else {
     return `${year} is not a leap year.`;
