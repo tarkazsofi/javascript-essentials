1aeabedee6b946255b696c9af9ddae000918576d
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-05-20 16:16:21 +0200

    Fixed calculator

--- is-it-your-birthday.js
+++ is-it-your-birthday.js
@@ -1,11 +1,15 @@
 const now = new Date();
+const month = now.getMonth();
+const day = now.getDate();
 const isItMyBirthday = (birthday) => {
   const birthdayDate = new Date(birthday);
-  if (birthdayDate === now) {
+  const birthdayMonth = birthdayDate.getMonth();
+  const birthdayDay = birthdayDate.getDate();
+  if (birthdayMonth === month && birthdayDay === day) {
     return "It is your birthday today. Happy birthday! 🍰";
   } else {
     return "It isn't your birthday today.";
   }
 };
-console.log(isItMyBirthday("1989-01-20"));
+console.log(isItMyBirthday("1989-05-20"));
 // Should output "It isn't your birthday today." or "It is your birthday today. Happy birthday! 🍰"
