6c433e62fe0fa200b11b24cc8e1167b299dc9ba7
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-05-25 10:42:18 +0200

    Fixed leap year calculations

--- is-it-a-leap-year.js
+++ is-it-a-leap-year.js
@@ -1,10 +1,18 @@
 const isItALeapYear = (year) => {
-  if (year % 4 === 0 && year % 400 === 0) {
-    return `${year} is a leap year.`;
-  } else {
+  if (year % 4 !== 0) {
     return `${year} is not a leap year.`;
+  }
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return `${year} is a leap year.`;
+  }
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) {
+    return `${year} is not a leap year.`;
+  }
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    return `${year} is a leap year.`;
   }
 };
 console.log(isItALeapYear(2021)); // Should output "2021 is not a leap year"
-
 console.log(isItALeapYear(2020)); // Should output "2020 is a leap year"
+console.log(isItALeapYear(2000));
+console.log(isItALeapYear(1900));
